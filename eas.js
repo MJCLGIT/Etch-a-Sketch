0543c624a43a579ab8e9ec565d4946746d95c3a1
MJCL <joule_patrols01@icloud.com>
2023-04-27 15:10:10 +0100

    [feat] create style for individual divs. \ \n\ Previously style was being applied to the container holding all 16 divs. Now the style is directly applied to the individual divs to identify them.

--- eas.js
+++ eas.js
@@ -2,6 +2,7 @@ let gridContainers = document.getElementById('gridContainers');
 
 for (let i = 1; i <= 16; i++){
     let newDiv = document.createElement('div');
-    newDiv.textContent = "Created Div " + i;
+    newDiv.setAttribute ('class', 'createdDivs')
+    newDiv.textContent = i;
     gridContainers.appendChild(newDiv);
 }
