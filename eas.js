fd12503aa3c01e8c7bcb769a06588da555334ed8
MJCL <joule_patrols01@icloud.com>
2023-04-27 16:26:50 +0100

    [feat] create hover effect over grid. Issue - responds to click only rather than simply passing over.

--- eas.js
+++ eas.js
@@ -1,8 +1,18 @@
+
+// 16 containers
 let gridContainers = document.getElementById('gridContainers');
 
 for (let i = 1; i <= 16; i++){
     let newDiv = document.createElement('div');
-    newDiv.setAttribute ('class', 'createdDivs')
-    newDiv.textContent = i;
+    newDiv.classList.add ('gridItem');
+    // newDiv.textContent = i;
     gridContainers.appendChild(newDiv);
+
+    // even listeners for hover effect
+    newDiv.addEventListener('mouseover', function() {
+        newDiv.classList.add('hover');
+    });
+    newDiv.addEventListener('mouseout', function() {
+        newDiv.classList.remove('hover');
+    });
 }
